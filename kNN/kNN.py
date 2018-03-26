739b8fa3ead1e79d723e89587d45daf61934a820
Piotr Zielinski <piotr_zielu_zielinski@yahoo.com>
2018-03-26 09:55:48 +0200

    Added parser; Added k-NN

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -1,31 +1,49 @@
 import operator
+from collections import Counter
 
 
-class struct_neighbour(object):
+class StructNeighbour(object):
     def __init__(self, vector, text):
-        super(struct_neighbour, self).__init__()
-        self.v = vector
-        self.t = text
+        super(StructNeighbour, self).__init__()
+        self.vector = vector
+        self.text = text
 
 
-def getNeighbours(trainingValues, testValue, k, distanceFunction):
+def distance_pitagoras2(a, b):
+    a_b_distance = 0
+    for point_a, point_b in zip(a.vector, b.vector):
+        a_b_distance += (point_a - point_b)**2
+    return a_b_distance
+
+
+def get_neighbours(trainingValues, testValue, k, distance_function):
     distances = []
     for x in range(len(trainingValues)):
-        dist = distanceFunction(testValue, trainingValues[x], len(testValue)-1)
-        distances += [[trainingValues[x], dist]]
+        dist = distance_function(testValue, trainingValues[x])
+        distances += [[trainingValues[x].text, dist]]
     distances.sort(key=operator.itemgetter(1))
     neighbors = []
     for x in range(k):
-        neighbors.append(distances[x][0])
+        neighbors += [distances[x][0]]
     return neighbors
 
-def kNN(unknown_xy, neighbours):
+
+def kNN(trainingValues, testValue, k, distance_function):
     """
         name, x, y
     """
-    odleglosci = []
-    najblizsze = dict()
-    for neighbour in neighbours:
-        odleglosci += [[neighbour.name, (neighbour.x-unknown_xy.x)**2 + (neighbour.y-unknown_xy.y)**2]]
-    najblizsze += sorted(odleglosci.items(), key=operator.itemgetter(1))
+    n = get_neighbours(trainingValues, testValue, k, distance_function)
+    return max(Counter(n).items(), key=operator.itemgetter(1))
 
+
+a = StructNeighbour([1, 2, 1], "a")
+b = StructNeighbour([9, 1, 5], "a")
+c = StructNeighbour([5, 8, 8], "a")
+d = StructNeighbour([1, 4, 1], "b")
+e = StructNeighbour([1, 1, 1], "b")
+f = StructNeighbour([4, 0, 3], "c")
+g = StructNeighbour([3, 4, 7], "d")
+training = [a,b,c,d,e,f,g]
+test = StructNeighbour([0, 0, 0], "test")
+whoami = kNN(training, test, 3, distance_pitagoras2)
+pass
---
+++ reutersParser.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+
+from bs4 import BeautifulSoup
+
+VALID_PLACES = ['west-germany', 'usa', 'france', 'uk', 'canada', 'japan']
+
+
+def is_reuter_valid(reuter):
+    if reuter.find('text').find('body') is None:
+        return False
+    try:
+        for place in reuter.places.find_all('d'):
+            if place.text in VALID_PLACES:
+                return True
+    except TypeError:  # NoneType
+        pass
+    return False
+
+
+new_soup = BeautifulSoup("<document></document>", "html.parser")
+
+for reuters_number_str in [str(i).zfill(3) for i in range(22)]:
+    with open("reuters21578/reut2-%s.sgm" % reuters_number_str) as fp:
+        print("Opened reuters21578/reut2-%s.sgm" % reuters_number_str)
+        soup = BeautifulSoup(fp, "html.parser")
+    reuters = soup.find_all('reuters')
+
+    for reuter in reuters:
+        if is_reuter_valid(reuter):
+            new_tag_reuters = new_soup.new_tag("reuters")
+            new_tag_places = new_soup.new_tag("places")
+            new_tag_body = new_soup.new_tag("body")
+
+            new_tag_places.append(reuter.places)
+            new_tag_body.append(reuter.find('text').find('body').text)
+
+            new_tag_reuters.append(new_tag_places)
+            new_tag_reuters.append(new_tag_body)
+            new_soup.document.append(new_tag_reuters)
+
+with open('reuters21578/reuters', 'w') as reuters_output:
+    reuters_output.write(new_soup.prettify())
