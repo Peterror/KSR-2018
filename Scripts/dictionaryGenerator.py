5b3a24ca5c78ac11717cc64211a7be9297c550e7
Piotr Zielinski <piotr_zielu_zielinski@yahoo.com>
2018-04-05 21:03:49 +0200

    podzielono zbior danych na test(33%) i train(66%); dodano slownik z suma sum wystepowan wszystkich wyrazow ze slownika(uff); slowniki uwzgledniaja tylko slowa bez interpunkcji(regex)

---
+++ Scripts/dictionaryGenerator.py
@@ -0,0 +1,35 @@
+import operator
+from collections import Counter
+from bs4 import BeautifulSoup
+import string
+import re
+
+with open("reuters21578/reuters_train") as fp:
+    print("Opened reuters21578/reuters_train")
+    soup = BeautifulSoup(fp, "html.parser")
+
+word_list = []
+
+#  trans_table = str.maketrans("", "", string.punctuation+string.digits)
+r = re.compile(r"[^a-zA-Z' ']")  # REGEX do wyciagania wszystkich znakow niechcianych(+ cyfr)
+
+for text in soup.find("document").find_all("body"):
+    word_list += r.sub('', text.text.lower()).split()  # lista slow wystepujacych w tekscie
+
+word_count = sorted(Counter(word_list).items(), key=operator.itemgetter(1), reverse=True)[:2000]
+del word_list
+
+new_soup = BeautifulSoup("<dict></dict>", "html.parser")
+
+frequency_sum = 0
+for word, frequency in word_count:
+    frequency_sum += frequency
+    new_tag = new_soup.new_tag("word")
+    new_tag.string = word
+    new_tag['frequency'] = frequency
+    new_soup.dict.append(new_tag)
+new_soup.dict['frequency_sum'] = frequency_sum
+
+with open('reuters21578/reuters_dictionary', 'w') as dictionary_output:
+    print('saving reuters_dictionary')
+    dictionary_output.write(new_soup.prettify())
